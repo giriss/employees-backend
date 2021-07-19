7877e68b68ede14cc7fd280ad1b07c7f781fac42
Girish Gopaul <gopaul0510@gmail.com>
2021-07-19 23:04:32 +0400

    Initial commit

---
+++ db/migrate/20210719185527_create_job_titles.rb
@@ -0,0 +1,10 @@
+class CreateJobTitles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :job_titles do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20210719185549_create_employees.rb
@@ -0,0 +1,15 @@
+class CreateEmployees < ActiveRecord::Migration[6.1]
+  def change
+    create_table :employees do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.date :dob
+      t.references :job_title, null: false, foreign_key: true
+      t.integer :status
+      t.boolean :permanent
+
+      t.timestamps
+    end
+  end
+end
