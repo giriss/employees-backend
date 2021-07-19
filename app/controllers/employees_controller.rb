80d01d20763625cda3041264c95132b8760a1212
Girish Gopaul <gopaul0510@gmail.com>
2021-07-19 23:06:22 +0400

    First rubocop autocorrect with -a

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,5 @@
 class EmployeesController < ApplicationController
-  before_action :set_employee, only: [:show, :update, :destroy]
+  before_action :set_employee, only: %i[show update destroy]
 
   # GET /employees
   def index
@@ -39,13 +39,14 @@ class EmployeesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_employee
-      @employee = Employee.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def employee_params
-      params.require(:employee).permit(:first_name, :last_name, :email, :dob, :job_title_id, :status, :permanent)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_employee
+    @employee = Employee.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :email, :dob, :job_title_id, :status, :permanent)
+  end
 end
--- spec/requests/employees_spec.rb
+++ spec/requests/employees_spec.rb
@@ -12,116 +12,116 @@ require 'rails_helper'
 # of tools you can use to make these specs even more expressive, but we're
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
-RSpec.describe "/employees", type: :request do
+RSpec.describe '/employees', type: :request do
   # This should return the minimal set of attributes required to create a valid
   # Employee. As you add validations to Employee, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) do
+    skip('Add a hash of attributes valid for your model')
+  end
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) do
+    skip('Add a hash of attributes invalid for your model')
+  end
 
   # This should return the minimal set of values that should be in the headers
   # in order to pass any filters (e.g. authentication) defined in
   # EmployeesController, or in your router and rack
   # middleware. Be sure to keep this updated too.
-  let(:valid_headers) {
+  let(:valid_headers) do
     {}
-  }
+  end
 
-  describe "GET /index" do
-    it "renders a successful response" do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       Employee.create! valid_attributes
       get employees_url, headers: valid_headers, as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
+  describe 'GET /show' do
+    it 'renders a successful response' do
       employee = Employee.create! valid_attributes
       get employee_url(employee), as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Employee" do
-        expect {
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Employee' do
+        expect do
           post employees_url,
                params: { employee: valid_attributes }, headers: valid_headers, as: :json
-        }.to change(Employee, :count).by(1)
+        end.to change(Employee, :count).by(1)
       end
 
-      it "renders a JSON response with the new employee" do
+      it 'renders a JSON response with the new employee' do
         post employees_url,
              params: { employee: valid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:created)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Employee" do
-        expect {
+    context 'with invalid parameters' do
+      it 'does not create a new Employee' do
+        expect do
           post employees_url,
                params: { employee: invalid_attributes }, as: :json
-        }.to change(Employee, :count).by(0)
+        end.to change(Employee, :count).by(0)
       end
 
-      it "renders a JSON response with errors for the new employee" do
+      it 'renders a JSON response with errors for the new employee' do
         post employees_url,
              params: { employee: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to eq("application/json")
+        expect(response.content_type).to eq('application/json')
       end
     end
   end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) do
+        skip('Add a hash of attributes valid for your model')
+      end
 
-      it "updates the requested employee" do
+      it 'updates the requested employee' do
         employee = Employee.create! valid_attributes
         patch employee_url(employee),
               params: { employee: new_attributes }, headers: valid_headers, as: :json
         employee.reload
-        skip("Add assertions for updated state")
+        skip('Add assertions for updated state')
       end
 
-      it "renders a JSON response with the employee" do
+      it 'renders a JSON response with the employee' do
         employee = Employee.create! valid_attributes
         patch employee_url(employee),
               params: { employee: new_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:ok)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
 
-    context "with invalid parameters" do
-      it "renders a JSON response with errors for the employee" do
+    context 'with invalid parameters' do
+      it 'renders a JSON response with errors for the employee' do
         employee = Employee.create! valid_attributes
         patch employee_url(employee),
               params: { employee: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to eq("application/json")
+        expect(response.content_type).to eq('application/json')
       end
     end
   end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested employee" do
+  describe 'DELETE /destroy' do
+    it 'destroys the requested employee' do
       employee = Employee.create! valid_attributes
-      expect {
+      expect do
         delete employee_url(employee), headers: valid_headers, as: :json
-      }.to change(Employee, :count).by(-1)
+      end.to change(Employee, :count).by(-1)
     end
   end
 end
